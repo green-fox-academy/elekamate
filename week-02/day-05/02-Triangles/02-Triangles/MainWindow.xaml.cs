6df45bee31e01c3fce5b44d5d966c5148ddee994
Mate Elek <elekamate@gmail.com>
2018-05-18 11:34:51 +0200

    02 Triangle - kickoff

---
+++ week-02/day-05/02-Triangles/02-Triangles/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace _02_Triangles
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            var points = new List<Point>();
+
+            points.Clear();
+            points.Add(new Point(10, 10));
+            points.Add(new Point(40, 210));
+            points.Add(new Point(170, 190));
+            foxDraw.FillColor(Colors.Green);
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.DrawPolygon(points);
+        }
+    }
+
+}
