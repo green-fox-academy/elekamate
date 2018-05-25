221e81e6e2abb7e387ac6cb971190b34bb770a60
Mate Elek <elekamate@gmail.com>
2018-05-25 12:13:33 +0200

    two triangles done (w03d05e01-Triangles)

--- week-03/day-05/01-Triangles/01-Triangles/MainWindow.xaml.cs
+++ week-03/day-05/01-Triangles/01-Triangles/MainWindow.xaml.cs
@@ -19,17 +19,17 @@ namespace _01_Triangles
             double triangleOriginalHeight = 0.5 * Math.Sqrt(3) * triangleOriginalWidth;
             double triangleOriginalIndent = 25;
 
-            double triangleP1X = triangleOriginalIndent;
-            double triangleP1Y = triangleOriginalIndent;
-            double triangleP2X = triangleP1X + triangleOriginalWidth;
-            double triangleP2Y = triangleOriginalIndent;
-            double triangleP3X = triangleOriginalIndent + triangleOriginalWidth / 2;
-            double triangleP3Y = triangleOriginalIndent + triangleOriginalHeight;
+            double triangleOriginalP1X = triangleOriginalIndent;
+            double triangleOriginalP1Y = triangleOriginalIndent;
+            double triangleOriginalP2X = triangleOriginalP1X + triangleOriginalWidth;
+            double triangleOriginalP2Y = triangleOriginalIndent;
+            double triangleOriginalP3X = triangleOriginalIndent + triangleOriginalWidth / 2;
+            double triangleOriginalP3Y = triangleOriginalIndent + triangleOriginalHeight;
 
             points.Clear();
-            points.Add(new Point(triangleP1X, triangleP1Y));
-            points.Add(new Point(triangleP2X, triangleP2Y));
-            points.Add(new Point(triangleP3X, triangleP3Y));
+            points.Add(new Point(triangleOriginalP1X, triangleOriginalP1Y));
+            points.Add(new Point(triangleOriginalP2X, triangleOriginalP2Y));
+            points.Add(new Point(triangleOriginalP3X, triangleOriginalP3Y));
             foxDraw.FillColor(Colors.Green);
             foxDraw.StrokeColor(Colors.Black);
             foxDraw.DrawPolygon(points);
@@ -39,16 +39,32 @@ namespace _01_Triangles
 
         private void DrawTrinaglesIntoTriangles(List<Point> argPoints)
         {
+            var points = new List<Point>();
+            var foxDraw = new FoxDraw(canvas);
+
             double triangleOriginalWidth = argPoints[1].X - argPoints[0].X;
             double triangleOriginalHeight = 0.5 * Math.Sqrt(3) * triangleOriginalWidth;
 
+            double triangleP1X = (argPoints[1].X > argPoints[0].X) ?
+                argPoints[0].X + (argPoints[1].X - argPoints[0].X) / 2 :
+                argPoints[0].X + (argPoints[1].X - argPoints[0].X) / 2;
+            double triangleP1Y = argPoints[1].Y;
+            double triangleP2X = argPoints[2].X+ (argPoints[1].X - argPoints[2].X)/2;
+            double triangleP2Y = argPoints[1].Y+(argPoints[2].Y - argPoints[1].Y)/2;
+            double triangleP3X = argPoints[0].X + (argPoints[2].X - argPoints[0].X)/2;
+            double triangleP3Y = argPoints[2].Y+(argPoints[0].Y - argPoints[2].Y)/2;
 
-            double triangleP1X = triangleOriginalIndent +;
-            double triangleP1Y = triangleOriginalIndent;
-            double triangleP2X = triangleP1X + triangleOriginalWidth;
-            double triangleP2Y = triangleOriginalIndent;
-            double triangleP3X = triangleOriginalIndent + triangleOriginalWidth / 2;
-            double triangleP3Y = triangleOriginalIndent + triangleOriginalHeight;
+            points.Add(new Point(triangleP1X, triangleP1Y));
+            points.Add(new Point(triangleP2X, triangleP2Y));
+            points.Add(new Point(triangleP3X, triangleP3Y));
+            foxDraw.FillColor(Colors.White);
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.DrawPolygon(points);
+
+            if (triangleOriginalWidth > 30)
+            {
+                DrawTrinaglesIntoTriangles(points);
+            }
         }
     }
 }
