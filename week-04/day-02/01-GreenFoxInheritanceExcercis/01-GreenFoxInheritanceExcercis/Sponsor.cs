b19bbc271ee7777e2286c6c744faffb8513fc735
Mate Elek <elekamate@gmail.com>
2018-05-29 16:29:35 +0200

    Chore (w04d02e01) Sponsor class - started

---
+++ week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Sponsor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_GreenFoxInheritanceExcercis
+{
+    class Sponsor : Person
+    {
+        private int hiredStudents;
+        private string  company;
+
+
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm name, a age year old gender who represents company and hired hiredStudents students so far.");
+        }
+    }
+}
