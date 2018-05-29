f58a549615281bec4580132dd1d2c955b9b64a23
Mate Elek <elekamate@gmail.com>
2018-05-29 16:43:48 +0200

    Chore (w04d02e01) Sponsor class - finished

--- week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Sponsor.cs
+++ week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Sponsor.cs
@@ -8,13 +8,41 @@ namespace _01_GreenFoxInheritanceExcercis
 {
     class Sponsor : Person
     {
-        private int hiredStudents;
-        private string  company;
+        private int HiredStudents { get; set; }
+        private string Company { get; set; }
 
+        public Sponsor()
+        {
+            Name = "Jane Doe";
+            Age = 30;
+            Gender = "female";
+            Company = "Google";
+            HiredStudents = 0;
+        }
+
+        public Sponsor(string name, int age, string gender, string company)
+        {
+            HiredStudents = 0;
+            Name = name;
+            Age = age;
+            Gender = gender;
+            Company = company;
+        }
 
         public override void Introduce()
         {
-            Console.WriteLine($"Hi, I'm name, a age year old gender who represents company and hired hiredStudents students so far.");
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old gender who represents {Company} and hired {HiredStudents} students so far.");
         }
+
+        public void Hire()
+        {
+            HiredStudents++;
+        }
+
+        public override void GetGoal()
+        {
+            Console.WriteLine("Hire brilliant junior software developers.");
+        }
+
     }
 }
