302e495730fe69340a49dfcce243925e904147da
Mate Elek <elekamate@gmail.com>
2018-05-29 16:24:11 +0200

    Chore (w04d02e01) Mentor class - finished

--- week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Mentor.cs
+++ week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Mentor.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace _01_GreenFoxInheritanceExcercis
 {
-    class Mentor
+    class Mentor : Person
     {
+        private static List<string> levelValues = new List<string>();
+
+        private string level;
+
+        public Mentor()
+        {
+            Name = "Jane Doe";
+            Age = 30;
+            Gender = "female";
+            Level = "intermediate";
+        }
+
+        public Mentor(string name, int age, string gender, string level)
+        {
+            Name=name;
+            Age = age;
+            Gender = gender;
+            Level = level;
+        }
+
+        public string Level
+        {
+            get
+            {
+                return level;
+            }
+
+            set
+            {
+                levelValuesListFill();
+                
+                if (levelValues.Contains(value) == true)
+                {
+                    level = value;
+                }        
+            }
+        }
+
+        public override void GetGoal()
+        {
+            Console.WriteLine("Educate brilliant junior software developers.");
+        }
+
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old {Gender} {Level} mentor.");
+        }
+
+        private static void levelValuesListFill()
+        {
+            levelValues.Add("junior");
+            levelValues.Add("intermediate");
+            levelValues.Add("senior");
+        }
     }
 }
