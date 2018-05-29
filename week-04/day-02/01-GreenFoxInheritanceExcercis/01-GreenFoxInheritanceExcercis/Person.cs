249da1b24d47c0983ce7c6d3556dd7bac4b00ac9
Mate Elek <elekamate@gmail.com>
2018-05-29 13:24:56 +0200

    Chore (w04d02e01) Person class-methods added

--- week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Person.cs
+++ week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Person.cs
@@ -11,6 +11,7 @@ namespace _01_GreenFoxInheritanceExcercis
         private int age;
         private string gender;
 
+
         public string Name { get; set; }
         public int Age
         {
@@ -18,24 +19,31 @@ namespace _01_GreenFoxInheritanceExcercis
 
             set
             {
-                if (value<1 || value>150)
+                if (value < 1 || value < 150)
                 {
                     Console.WriteLine("Invalid age.");
                 }
                 else
                 {
-                    age=value;
+                    age = value;
                 }
             }
         }
+
         public string Gender
         {
             get { return gender; }
             set
             {
-
+                if ( value == "male" || value == "female" )
+                {
+                    gender = value;
+                }
+                else
+                {
+                    Console.WriteLine( "Invalid gender value." );
+                }
             }
-
         }
 
         public Person(string name, int age, string gender)
@@ -51,5 +59,10 @@ namespace _01_GreenFoxInheritanceExcercis
             this.Age = 30;
             this.Gender = "female";
         }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old {Gender}.");
+        }  
     }
 }
