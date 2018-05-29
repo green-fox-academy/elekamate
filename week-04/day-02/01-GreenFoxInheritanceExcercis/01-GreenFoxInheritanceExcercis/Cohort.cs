6dd23ae5cb393acd52defc5087d59c97dba15a3c
Mate Elek <elekamate@gmail.com>
2018-05-29 17:19:43 +0200

    Chore (w04d02e01) Cohort class - Finished

--- week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Cohort.cs
+++ week-04/day-02/01-GreenFoxInheritanceExcercis/01-GreenFoxInheritanceExcercis/Cohort.cs
@@ -8,5 +8,28 @@ namespace _01_GreenFoxInheritanceExcercis
 {
     class Cohort
     {
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Mentor> Mentors { get; set; }
+
+        public Cohort(string name)
+        {
+            Name = name;
+        }
+
+        public void AddStudent(Student argStudent)
+        {
+            Students.Add(argStudent);
+        }
+
+        public void AddMentor(Mentor argMentor)
+        {
+            Mentors.Add(argMentor);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"The {Name} cohort has {Students.Count()} students and {Mentors.Count}  mentors.");
+        }
     }
 }
