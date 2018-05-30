f6e08ff59158c7c0a42d6d8a826ba5fec7108777
Mate Elek <elekamate@gmail.com>
2018-05-30 13:05:00 +0200

    Chore(w04d03t01e01-Cloneable): classes imported from prev project

---
+++ week-04/day-03/01-Interface-01-Cloneable/01-Interface-01-Cloneable/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_GreenFoxInheritanceExcercis
+{
+    public class Person
+    {
+        // Fields
+        private int age;
+        private string gender;
+        // Properties
+        public string Name { get; set; }
+        public int Age
+        {
+            get{ return age; }
+
+            set
+            {
+                if (value < 1 || value > 150)
+                {
+                    Console.WriteLine("Invalid age.");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                if ( value == "male" || value == "female" )
+                {
+                    gender = value;
+                }
+                else
+                {
+                    Console.WriteLine( "Invalid gender value." );
+                }
+            }
+        }
+
+        // Construcures
+        public Person(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public Person()
+        {
+            this.Name = "Jane Doe";
+            this.Age = 30;
+            this.Gender = "female";
+        }
+
+        // Methods
+        public virtual void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old {Gender}.");
+        }
+
+        public virtual void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+
+    }
+}
---
+++ week-04/day-03/01-Interface-01-Cloneable/01-Interface-01-Cloneable/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_GreenFoxInheritanceExcercis
+{
+    class Student : Person
+    {
+        // Properties
+        public int SkippedDays { get; set; }
+        public string PreviousOrganization { get; set; }
+
+        // Constructures
+        public Student(string name, int age, string gender, string previousOrganization) 
+            : base(name,age,gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            PreviousOrganization = previousOrganization;
+            SkippedDays = 0;
+        }
+
+        public Student()
+        {
+            Name = "Jane Doe";
+            Age = 30;
+            Gender = "female";
+            PreviousOrganization = "The School of Life";
+            SkippedDays = 0;
+        }
+
+        // Methods
+        public override void GetGoal()
+        {
+            Console.WriteLine("My goal is: Be a junior software developer.");
+        }
+
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old {Gender}" +
+                $" from {PreviousOrganization} who skipped {SkippedDays}" +
+                $" days from the course already.");
+        }
+
+        public void SkipDays(int numberOfDays)
+        {
+            SkippedDays += numberOfDays;
+        }
+
+    }
+}
