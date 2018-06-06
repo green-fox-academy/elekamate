de2935958e6810e45cb40ad7adbb8f28665f4cea
Mate Elek <elekamate@gmail.com>
2018-06-06 15:54:53 +0200

    Feat(w02d05-Lottery): Function working w/ infinit loop.

--- week-02/day-03/Optional-02-Lotery/Optional-02-Lottery/Optional-02-Lottery/Program.cs
+++ week-02/day-03/Optional-02-Lotery/Optional-02-Lottery/Optional-02-Lottery/Program.cs
@@ -14,6 +14,9 @@ namespace Optional_02_Lottery
             // Create a method that find the 5 most common lottery numbers in lottery.csv
             string lotteryDatafilePath = "lottery.csv";
             List<int> lotteryNumbers = ImportLotteryNumbersToList(lotteryDatafilePath);
+            Dictionary<int, int> lotteryNumberFreq = CreateNumberFreqDict(lotteryNumbers);
+            List<int> top5LotteryNumbers = Findtop5LotteryNumbers(lotteryNumberFreq);
+
             Console.ReadLine();
         }
 
@@ -21,16 +24,80 @@ namespace Optional_02_Lottery
         {
             string[] lotteryDataRaw = File.ReadAllLines(argLotteryDatafilePath);
             List<int> lotteryNumbers = new List<int>();
+            lotteryNumbers = LoadLotteryNumbersToList(lotteryDataRaw);
+            return lotteryNumbers;
+        }
+
+        private static List<int> LoadLotteryNumbersToList(string[] lotteryDataRaw)
+        {
+            List<int> lotteryNumbers = new List<int>();
             string[] tempLineContent;
             foreach (string xLine in lotteryDataRaw)
             {
                 tempLineContent = xLine.Split(';');
-                for (int i = tempLineContent.Length-1; i > tempLineContent.Length - 6; i--)
+                for (int i = tempLineContent.Length - 1; i > tempLineContent.Length - 6; i--)
                 {
                     lotteryNumbers.Add(Convert.ToInt32(tempLineContent[i]));
                 }
             }
             return lotteryNumbers;
+        }
+
+        private static Dictionary<int, int> CreateNumberFreqDict(List<int> argLotteryNumbers)
+        {
+            Dictionary<int, int> lotteryNumberFreq = new Dictionary<int, int>();
+            foreach (int xNumber in argLotteryNumbers)
+            {
+                if (lotteryNumberFreq.ContainsKey(xNumber))
+                {
+                    lotteryNumberFreq[xNumber]++;
+                }
+                else
+                {
+                    lotteryNumberFreq.Add(xNumber, 1);
+                }
+            }
+            return lotteryNumberFreq;
+        }
+
+        private static List<int> Findtop5LotteryNumbers(Dictionary<int, int> arglotteryNumberFreq)
+        {
+            List<int> top5LotteryNumbers = new List<int>();
+            int prevFreq=0;
+            int givenFreq = 0;
+            int givenKey = 0;
+
+            foreach (var xKey in arglotteryNumberFreq.Keys)
+            {
+                if (arglotteryNumberFreq.Values.Max() != arglotteryNumberFreq[xKey])
+                {
+
+                }
+            }
+            while (0==0)
+            {
+                foreach (var xKey in arglotteryNumberFreq.Keys)
+                {
+                    if (arglotteryNumberFreq.Values.Max() == arglotteryNumberFreq[xKey])
+                    {
+                        top5LotteryNumbers.Add(xKey);
+                        givenKey = xKey;
+                        prevFreq = givenFreq;
+                        givenFreq = arglotteryNumberFreq[xKey];
+                        arglotteryNumberFreq.Remove(xKey);
+                        break;
+                    }
+                }
+                if (top5LotteryNumbers.Count > 4 && prevFreq != givenFreq)
+                {
+                    if (top5LotteryNumbers.Count > 5)
+                    {
+                        top5LotteryNumbers.Remove(givenKey);
+                    }
+                    break;
+                }
+            }
+            return top5LotteryNumbers;
         }
     }
 }
