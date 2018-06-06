2fdc209936aabda1b0c35d2c2f8cb22fd8353578
Mate Elek <elekamate@gmail.com>
2018-06-06 20:40:11 +0200

    Feat(w04d04e02-Sum): Scenarios added

--- week-04/day-04/02-sum/UnitTestProject1/UnitTest1.cs
+++ week-04/day-04/02-sum/UnitTestProject1/UnitTest1.cs
@@ -8,13 +8,36 @@ namespace UnitTestProject1
     [TestFixture]
     public class UnitTest1
     {
-        [Test]
-        public static void TestMethod1()
+        Calc calc;
+
+        //[SetUp]
+        //public void SetUp()
+        //{
+        //    calc = new Calc();
+        //}
+
+        [TestCase(10)]
+        public static void TestForMultiElement(int inputNumbers)
         {
-            List<int> inputNumbers = new List<int> { 1, 2, 3, 4 };
-            int expectedOutput = 10;
-            int calcOutput = Calc.SumNumbers(inputNumbers);
-            Assert.AreEqual(expectedOutput, calcOutput);
+            Assert.AreEqual(inputNumbers, Calc.SumNumbers(new List<int> { 1, 2, 3, 4 }));
+        }
+
+        [TestCase(0)]
+        public static void TestForEmpty(int inputNumbers)
+        {
+            Assert.AreEqual(inputNumbers, Calc.SumNumbers(new List<int> {  }));
+        }
+
+        [TestCase(15)]
+        public static void TestForOneElement(int inputNumbers)
+        {
+            Assert.AreEqual(inputNumbers, Calc.SumNumbers(new List<int> { 15 }));
+        }
+
+        [TestCase(15)]
+        public static void TestForNull(int inputNumbers)
+        {
+            Assert.AreEqual(inputNumbers, Calc.SumNumbers(new List<int> { null }));
         }
     }
 }
