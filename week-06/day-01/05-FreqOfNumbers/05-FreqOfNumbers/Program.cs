11a610b203b59839346e204c5b982555468871c7
Mate Elek <elekamate@gmail.com>
2018-06-20 14:25:26 +0200

    w06d02 finished, w06d03 started

---
+++ week-06/day-01/05-FreqOfNumbers/05-FreqOfNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_FreqOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var freqOfNumbers = from number in n
+                                            orderby number
+                                             group number by number into numberFreq
+                                             select numberFreq;
+
+            var freqOfNumbersLINQ = n
+                .OrderBy(x => x)
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var number in freqOfNumbers)
+            {
+                Console.WriteLine($"{number.Key}: {number.Count()}");
+            }
+
+            foreach (var number in freqOfNumbersLINQ)
+            {
+                Console.WriteLine($"{number.Key}: {number.Count()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-06/day-02/Q6 answer.cs
@@ -0,0 +1,28 @@
+﻿//https://lagunita.stanford.edu/courses/DB/SQL/SelfPaced/courseware/ch-sql/seq-exercise-sql_movie_query_core/
+
+SELECT
+    reviewer.name, movie.title
+FROM
+(
+    SELECT
+        ra2.rID, ra2.mID
+    FROM
+        rating ra1, rating ra2 
+    WHERE
+        ra1.mID = ra2.mID
+        AND
+        ra1.rID = ra2.rID
+        AND
+        ra1.ratingdate < ra2.ratingdate
+        AND
+        ra1.stars < ra2.stars
+) as t1
+INNER JOIN
+    movie
+ON
+    movie.mID = t1.mID
+INNER JOIN
+    reviewer
+ON
+    reviewer.rID = t1.rID
+;
