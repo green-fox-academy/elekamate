84f5b4851fea1a95d367753deca04ac5041c1bd3
Mate Elek <elekamate@gmail.com>
2018-06-21 18:03:52 +0200

    refact(Todo-app-SQL): Database command has setted up.

---
+++ week-06/day-04/Todo-app rework/Todo-app rework/DataBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+using System.IO;
+
+namespace ToDoApp
+{
+    class DataBase
+    {
+        private SQLiteConnection myConnection;
+
+
+        public DataBase()
+        {
+            myConnection = new SQLiteConnection("Data Source = todoDB.sqlite3");
+            if (!File.Exists("todoDB.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("todoDB.sqlite3");
+                Console.WriteLine("Todo DB has not found, file created.");
+            }
+            else
+            {
+                Console.WriteLine("Todo DB file already exists, it will be used.");
+            }
+        }
+
+        public SQLiteConnection GetMyConnection()
+        {
+            return myConnection;
+        }
+
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ week-06/day-04/Todo-app rework/Todo-app rework/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase todoDB = new DataBase();
+
+            string query = "INSERT INTO album ('title', 'artist') VALUES (@title, @artist)";
+            SQLiteCommand myCommand = new SQLiteCommand(query, todoDB.GetMyConnection());
+            todoDB.OpenConnection();
+            myCommand.Parameters.AddWithValue("@title", "xx");
+            myCommand.Parameters.AddWithValue("@artist", "yy");
+
+            var result = myCommand.ExecuteNonQuery();
+            todoDB.CloseConnection();
+
+            Console.WriteLine($"Entries added: {result}");
+
+
+
+
+            //string query2 = "SELECT * FROM albums";
+            //SQLiteCommand myCommand2 = new SQLiteCommand(query2, todoDB.GetMyConnection());
+            //todoDB.OpenConnection();
+            //SQLiteDataReader result2 = myCommand2.ExecuteReader();
+            //if (result2.HasRows)
+            //{
+            //    while (result2.Read())
+            //    {
+            //        Console.WriteLine($"Album: {result2["title"]}, {result2["artist"]}");
+            //    }
+            //}
+            //todoDB.CloseConnection();
+
+            Console.WriteLine($"Entries added: {result}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
