0e0e90c37063633751ef3e99a0e8b3f154febc47
Mate Elek <elekamate@gmail.com>
2018-06-28 13:01:34 +0200

    Chore(w07d04e01-FoxClub): Index working.

---
+++ week-07/day-04/Fox Club Project/FoxClubProject/FoxClubProject/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoxClubProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/")]
+        public IActionResult PostLoginInfo()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
