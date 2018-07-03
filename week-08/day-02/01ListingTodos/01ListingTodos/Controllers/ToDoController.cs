49218d92e9ab8ad8bcbd0a53e515a466c58c5874
Mate Elek <elekamate@gmail.com>
2018-07-03 19:11:32 +0200

    w08d02 finished

---
+++ week-08/day-02/01ListingTodos/01ListingTodos/Controllers/ToDoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ListingTodos.Models;
+using ListingTodos.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListingTodos.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private ToDoContext toDoContext;
+
+        public ToDoController(ToDoContext toDoContext)
+        {
+            this.toDoContext = toDoContext;
+        }
+
+        public void Index()
+        {
+            Response.Redirect("list?isactive=true");
+        }
+
+        public IActionResult List(bool IsActive)
+        {
+            IEnumerable<ToDo> filteredToDoDB = (IsActive == true) ? toDoContext.ToDos.ToList().Where(toDo => toDo.IsDone == false) : toDoContext.ToDos.ToList();
+            return View(filteredToDoDB.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult AddToDo()
+        {
+            return View("AddToDo", toDoContext.ToDos);
+        }
+
+        [HttpPost]
+        public IActionResult PostToDo(string toDoTitle)
+        {
+            ToDo toDoToAdd = new ToDo()
+            {
+                Title = toDoTitle,
+                IsUrgent = false,
+                IsDone = false,
+            };
+            return Redirect("index");
+        }
+    }
+}
---
+++ week-08/day-02/01ListingTodos/01ListingTodos/Repositories/ToDoContext.cs
@@ -0,0 +1,18 @@
+﻿using ListingTodos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingTodos.Repositories
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<ToDo> ToDos { get; set; }
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+        {
+
+        }
+    }
+}
